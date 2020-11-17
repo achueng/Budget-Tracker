fc23e55d6270c11e3fce23a95179fe5b4adbedb9
Agnes Chueng <agneschueng@gmail.com>
2020-11-16 22:27:52 -0600

    create object store in indexedDB

--- public/db.js
+++ public/db.js
@@ -1,7 +1,9 @@
 const request = window.indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-    //
+    // creates an object store in indexedDB
+    const db = event.target.result;
+    db.createObjectStore("pending",{autoIncrement: true});
 };
 
 request.onsuccess = function (event) {
